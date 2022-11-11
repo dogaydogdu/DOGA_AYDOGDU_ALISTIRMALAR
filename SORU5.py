2f862de7533e408446bf293693e0fd14ac7633c9
Doğa Aydoğdu <114919559+dogaydogdu@users.noreply.github.com>
2022-11-11 14:55:56 +0300

    Update SORU5.py

--- SORU5.py
+++ SORU5.py
@@ -3,3 +3,8 @@ for i in range(1000,10000):
     l=k[::-1]
     if k==l:
         print(k)
+        #other method
+        for i in range(1000,10000):
+    k=str(i)
+    if str(k[0])==str(k[3]) and str(k[1])==str(k[2]):
+        print(k)
